770263aa9982b0a80e7b4ef2725bfcb8357c9aad
pawel150199 <pawel.polski99@gmail.com>
2022-05-28 18:00:19 +0200

    Metric Tests

--- MetricEvaluate.py
+++ MetricEvaluate.py
@@ -7,12 +7,13 @@ from imblearn.under_sampling import RandomUnderSampler, ClusterCentroids
 from ModifiedClusterCentroid import ModifiedClusterCentroids
 from strlearn.metrics import precision, specificity, f1_score, geometric_mean_score_1, balanced_accuracy_score
 
-""" Klasyfikatory uzyte w eksperymencie
-    GNB: GaussianNB
-    SVC: SVC
-    kNN: KNeighborsClassifier
-    Linear SVC: LinearSVC
-"""
+#Klasyfikatory uzyte w eksperymencie
+clfs={
+    'GNB': GaussianNB(),
+    'SVC': SVC(),
+    'kNN': KNeighborsClassifier(),
+    'Linear SVC': LinearSVC()
+}
 
 #metody undersampligu
 preprocs = {
@@ -28,66 +29,38 @@ mrts = {
     'bac': balanced_accuracy_score,
 }
 
-#walidacja krzyzowa
-n_splits = 5
-n_repeats = 2
+#zbiór danych
 datasets = ['yeast6', 'balance', 'australian']
-rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state = 1234)
 
-#tablice z wynikami
-scores_GNB = np.zeros((len(datasets), len(preprocs), n_splits*n_repeats, len(mrts)))
-scores_SVC = np.zeros((len(datasets), len(preprocs), n_splits*n_repeats, len(mrts)))
-scores_kNN = np.zeros((len(datasets), len(preprocs), n_splits*n_repeats, len(mrts)))
-scores_LSVC = np.zeros((len(datasets), len(preprocs), n_splits*n_repeats, len(mrts)))
+if __name__ =='__main__':
+    #walidacja krzyzowa
+    n_splits = 5
+    n_repeats = 2
+    rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state = 1234)
 
-#Eksperyment
-for data_id, data_name in enumerate(datasets):
-    dataset = np.genfromtxt("datasets/%s.csv" % (data_name) , delimiter=',')
-    X = dataset[:, :-1]
-    y = dataset[:, -1].astype(int)
+    #tablice z wynikami
+    scores = np.zeros((len(datasets), len(preprocs), n_splits*n_repeats, len(mrts), len(clfs)))
+    
+    #Eksperyment
+    for data_id, data_name in enumerate(datasets):
+        dataset = np.genfromtxt("datasets/%s.csv" % (data_name) , delimiter=',')
+        X = dataset[:, :-1]
+        y = dataset[:, -1].astype(int)
 
-    for fold_id, (train, test) in enumerate(rskf.split(X, y)):
-        for preproc_id, preproc_name in enumerate(preprocs):
-            if preprocs[preproc_name] == None:
-                X_res, y_res = X[train], y[train]
-            else:
-                X_res, y_res = preprocs[preproc_name].fit_resample(X[train],y[train])
+        for fold_id, (train, test) in enumerate(rskf.split(X, y)):
+            for preproc_id, preproc_name in enumerate(preprocs):
+                if preprocs[preproc_name] == None:
+                    X_res, y_res = X[train], y[train]
+                else:
+                    X_res, y_res = preprocs[preproc_name].fit_resample(X[train],y[train])
 
-            #GNB
-            clf_GNB = GaussianNB()
-            clf_GNB.fit(X_res, y_res)
-            y_pred = clf_GNB.predict(X[test])
-            for m_id, m_name in enumerate(mrts):
-                mtr = mrts[m_name]
-                scores_GNB[data_id, preproc_id, fold_id, m_id] = mtr(y[test],y_pred)
+                for clf_id, clf_name in enumerate(clfs):
+                    clf_GNB = clfs[clf_name]
+                    clf_GNB.fit(X_res, y_res)
+                    y_pred = clf_GNB.predict(X[test])
+                    for m_id, m_name in enumerate(mrts):
+                        mtr = mrts[m_name]
+                        scores[data_id, preproc_id, fold_id, m_id, clf_id] = mtr(y[test],y_pred)
 
-            #SVC
-            clf_SVC = SVC()
-            clf_SVC.fit(X_res, y_res)
-            y_pred = clf_SVC.predict(X[test])
-            for m_id, m_name in enumerate(mrts):
-                mtr = mrts[m_name]
-                scores_GNB[data_id, preproc_id, fold_id, m_id] = mtr(y[test],y_pred)
-
-            #kNN
-            clf_kNN = KNeighborsClassifier()
-            clf_kNN.fit(X_res, y_res)
-            y_pred = clf_kNN.predict(X[test])
-            for m_id, m_name in enumerate(mrts):
-                mtr = mrts[m_name]
-                scores_GNB[data_id, preproc_id, fold_id, m_id] = mtr(y[test],y_pred)
-
-                
-            #LSVC
-            clf_LSVC = LinearSVC(random_state=1234, tol=1e-5)
-            clf_LSVC.fit(X_res, y_res)
-            y_pred = clf_LSVC.predict(X[test])
-            for m_id, m_name in enumerate(mrts):
-                mtr = mrts[m_name]
-                scores_GNB[data_id, preproc_id, fold_id, m_id] = mtr(y[test],y_pred)
-
-#zapisanie  wyników 
-np.save('metric_results_GNB', scores_GNB)
-np.save('metric_results_SVC', scores_SVC)
-np.save('metric_results_kNN', scores_kNN)
-np.save('metric_results_LSVC', scores_LSVC)
+    #zapisanie  wyników 
+    np.save('metric_results', scores)
--- MetricTests.py
+++ MetricTests.py
@@ -1,13 +1,8 @@
+from matplotlib.pyplot import axis
 import numpy as np
 from tabulate import tabulate
 from scipy.stats import ttest_ind
-from imblearn.under_sampling import RandomUnderSampler, ClusterCentroids
-from ModifiedClusterCentroid import ModifiedClusterCentroids
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC, LinearSVC
-from sklearn.neighbors import KNeighborsClassifier
-from strlearn.metrics import recall, precision, specificity, f1_score, geometric_mean_score_1, balanced_accuracy_score
-from MetricEvaluate import mrts, preprocs, datasets
+from MetricEvaluate import mrts, preprocs, datasets, clfs
 """ Klasyfikatory uzyte w eksperymencie
     GNB: GaussianNB
     SVC: SVC
@@ -22,44 +17,50 @@ std_fmt=None
 nc="---"
 db_fmt="%s"
 tablefmt="plain"
+metrics = list(mrts.keys())
+preprocs = list(preprocs.keys())
+n_preprocs = len(preprocs)
 
 if __name__=="__main__":
     #import wyników
-    scores = np.load("metric_results_GNB.npy")
-    mean_scores = np.mean(scores, axis=2)
-    stds = np.std(scores, axis=2)
-    clfs = list(preprocs.keys())
-    n_clfs = len(preprocs)
-    # Perform tests
-    tables = {}
-    for m_idx, m_name in enumerate(preprocs):
-        # Prepare storage for table
+
+    for clf_id, clf_name in enumerate(clfs):
+        scores = np.load("metric_results.npy")
+        scores = scores[:,:,:,:,clf_id]
+        mean_scores = np.mean(scores, axis=2)
+        stds = np.std(scores, axis=2)
+
+        # Perform tests
+        tables = {}
         t = []
-        for db_idx, db_name in enumerate(datasets):
-            # Row with mean scores
-            t.append([db_fmt % db_name] + [m_fmt % v for v in mean_scores[db_idx, m_idx, :]])
-            # Row with std
-            if std_fmt:
-                t.append([''] + [std_fmt %
-                                 v for v in
-                                 stds[db_idx, :, m_idx]])
-            # Calculate T and p
-            T, p = np.array(
-                [[ttest_ind(scores[db_idx, m_idx, i, :],
-                       scores[db_idx, m_idx, j, :])
-                  for i in range(len(clfs))]
-                 for j in range(len(clfs))]
-            ).swapaxes(0, 2)
-            _ = np.where((p < alpha) * (T > 0))
-            conclusions = [list(1 + _[1][_[0] == i])
-                           for i in range(n_preprocs)]
-            # Row with conclusions
-            # t.append([''] + [", ".join(["%i" % i for i in c])
-            #                  if len(c) > 0 else nc
-            #                  for c in conclusions])
-            t.append([''] + [", ".join(["%i" % i for i in c])
-                             if len(c) > 0 and len(c) < len(preprocs)-1 else ("all" if len(c) == len(preprocs)-1 else nc)
-                             for c in conclusions])
-        # Store formatted table
-        tables.update({m_name: tabulate(
-            t, headers=['DATASET'] + preprocs, tablefmt=tablefmt)})
+        for m_idx, m_name in enumerate(metrics):
+            # Prepare storage for table
+            for db_idx, db_name in enumerate(datasets):
+                # Row with mean scores
+                t.append(['%s' % m_name]+ [db_fmt % db_name] + [m_fmt % v for v in mean_scores[db_idx, :, m_idx]])
+                # Row with std
+                if std_fmt:
+                    t.append(['']+[''] + [std_fmt % v for v in stds[db_idx, :, m_idx]])
+                # Calculate T and p
+                T, p = np.array(
+                    [[ttest_ind(scores[db_idx, i, :, m_idx],
+                        scores[db_idx, j, :, m_idx])
+                    for i in range(len(preprocs))]
+                    for j in range(len(preprocs))]
+                ).swapaxes(0, 2)
+                _ = np.where((p < alpha) * (T > 0))
+                conclusions = [list(1 + _[1][_[0] == i])
+                            for i in range(n_preprocs)]
+        
+                t.append(['']+[''] + [", ".join(["%i" % i for i in c])
+                                if len(c) > 0 and len(c) < len(preprocs)-1 else ("all" if len(c) == len(preprocs)-1 else nc)
+                                for c in conclusions])
+
+            # Store formatted table
+        print('\n\n\n', clf_name, '\n')  
+        headers = ['metrics', 'datasets']
+        for i in preprocs:
+            headers.append(i)
+        print(headers)
+        print(tabulate(t))
+        
